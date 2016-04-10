3d146159fa07c08d31e1a2095afb2325c8fa4c59
Sai Krishna <saikrishna321@yahoo.com>
2016-04-10 17:24:57 +0530

    fixed broken report

--- src/test/java/com/cucumber/stepdefinitions/MyStepdefs.java
+++ src/test/java/com/cucumber/stepdefinitions/MyStepdefs.java
@@ -10,13 +10,13 @@ public class MyStepdefs {
 
     @Given("I have (\\d+) cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
-        System.out.format("Cukes: %n\n", cukes);
+        System.out.format("Cukes:"+cukes);
 //        Assert.assertTrue(false);
     }
 
     @Given("I have (\\d+) cukes in my bellies")
     public void I_have_cukes_in_my_bellies(int cukes) {
-        System.out.format("Cukes: %n\n", cukes);
+        System.out.format("Cukes:"+cukes);
     }
 
     @Then("^I print$")
